585ae251a584541ab46c2f2350f68337f670286f
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-04-20 19:55:36 +0200

    Skapa funktion som tar bort resultaträknare för Standardsorteringen

--- themes/storefront-child/functions.php
+++ themes/storefront-child/functions.php
@@ -7,4 +7,12 @@ function remove_sidebar() {
     
 add_filter( 'is_active_sidebar', 'remove_sidebar', 10, 2 ); 
 
+//Tar bort texten Visar alla 5 resultat vid Standardsorteringen
+function my_remove_product_result_count() {
+    remove_action('woocommerce_before_shop_loop', 'woocommerce_result_count', 20);
+    remove_action('woocommerce_after_shop_loop', 'woocommerce_result_count', 20);
+}
+
+add_action('after_setup_theme', 'my_remove_product_result_count', 99);
+
 ?>
