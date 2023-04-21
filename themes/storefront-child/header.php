110bcfedc607e4c264e681137e4d1fe664d60b8a
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-04-21 10:55:42 +0200

    CSS för undermenyn

--- themes/storefront-child/header.php
+++ themes/storefront-child/header.php
@@ -54,7 +54,7 @@
         'menu' => 'undermeny',
         'container' => '',
         'theme_location' => 'undermeny',
-        'items_wrap' => '<ul class="menu">%3$s</ul>'
+        'items_wrap' => '<ul class="menu2">%3$s</ul>'
     ));
     ?>
 
